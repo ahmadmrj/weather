30a3e94a2b398dbfed6c631e63e5c4f4396b3288
ahmad jafari <ahmad.mrj@gmail.com>
2019-12-19 08:54:03 +0330

    init

---
+++ src/Controller/PredictionController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use App\Service\PredictionService;
+
+class PredictionController
+{
+	public function test(PredictionService $prediction) {
+		return new Response(
+			'hello wold!'.$prediction->getPrediction()
+		);
+	}
+}
---
+++ src/Service/PredictionService.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Service;
+
+class PredictionService
+{
+	public function getPrediction() {
+		return 'yes.';
+	}
+}
